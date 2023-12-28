7c3208e6c0f0be18d6fb03190783363ac233e5af
penghs <penghs@agilean.cn>
2023-12-28 09:21:56 +0800

    插入排序

--- algorithm/src/main/java/sort/InsertionSort.java
+++ algorithm/src/main/java/sort/InsertionSort.java
@@ -15,11 +15,14 @@ public class InsertionSort {
         // 再做到0~2范围有序
         // 最后0~n-1范围有序
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i ; j > 0; j--) {
+            /*for (int j = i ; j > 0; j--) {
                 //如果当前的数比左边的小，则交换
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
                 }
+            }*/
+            for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+                swap(arr, j, j - 1);
             }
         }
     }
@@ -31,7 +34,7 @@ public class InsertionSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 3, 2, 5, 4};
+        int[] arr = {7, 1, 23, 0, 1, 3, 2, 5, 4};
         new InsertionSort().sort(arr);
         for (int i : arr) {
             System.out.println(i);
